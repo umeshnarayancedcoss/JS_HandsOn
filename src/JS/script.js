2f8d67679e96bf39f221fdedfbac509b34207772
umeshnarayancedcoss <umeshnarayan@cedcoss.com>
2022-09-26 12:51:24 +0530

    JS string methods Exercise 3

--- src/JS/script.js
+++ src/JS/script.js
@@ -139,3 +139,9 @@ toUpperCase();
 let txt = "I can eat bananas all day";
 let x = txt.slice(10, 17);
 }
+
+{
+    // JS String Methods exercise 3
+    let txt = "Hello World";
+txt = txt.replace("Hello", "Welcome");
+}
