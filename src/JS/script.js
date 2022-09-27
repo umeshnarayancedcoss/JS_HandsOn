cb16408a8ebf9865eb04ee57716c0c9eea3d0c7c
umeshnarayancedcoss <umeshnarayan@cedcoss.com>
2022-09-27 11:54:51 +0530

    JS Arrays exercise 1

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,41 +1,41 @@
 // JS Variables Exercise 1
-var carName= "Volvo";
+var carName = "Volvo";
 
 // JS variables Exercise 2
-var x= 50
+var x = 50
 
 // JS Variables Exercise 3
-var x= 5;
+var x = 5;
 var y = 10;
-document.getElementById("demo").innerHTML = x+y;
+document.getElementById("demo").innerHTML = x + y;
 
 // JS variables exercise 4
 var x = 5;
 var y = 10;
 var z = x + y;
-alert (z);
+alert(z);
 
 // JS Variables exercise 5
-var firstName= "John",lastName = "Doe",age= 35;
+var firstName = "John", lastName = "Doe", age = 35;
 
 // JS Operators exercise 1
-alert(10*5);
+alert(10 * 5);
 
 // JS Operators exercise 2
-alert(10/2);
+alert(10 / 2);
 
 // JS Operators exercise 3
-alert(15%9);
+alert(15 % 9);
 
 // JS Operators exercise 4
 x = 10;
 y = 5;
-x+= y;
+x += y;
 
 // JS Operators exercise 5
 x = 10;
 y = 5;
-x*= y;
+x *= y;
 
 // JS Datatypes Exercise 1
 let length = 16;          // Number
@@ -47,113 +47,118 @@ const x = {
 
 // JS FUNCTIONS exercise 1
 function myFunction() {
-    alert("Hello World!");
-  }
-  myFunction();
+  alert("Hello World!");
+}
+myFunction();
 
-  //JS Function exercise 2
-function myFunction() 
-{
-alert("Hello World!");
+//JS Function exercise 2
+function myFunction() {
+  alert("Hello World!");
 }
 
 
 // JS Functions exercise 3
 function myFunction() {
   return "Hello";
-    }
-    document.getElementById("demo").innerHTML = myFunction();
+}
+document.getElementById("demo").innerHTML = myFunction();
 
 // JS Functions exercise 4
-    function myFunction() {
-        document.getElementById("demo").innerHTML= "Hello";
-      }
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello";
+}
 {
-//   JS Objects exercise 1
-    const person = {
-        firstName: "John",
-        lastName: "Doe"
-    };
-      alert(person.firstName);
+  //   JS Objects exercise 1
+  const person = {
+    firstName: "John",
+    lastName: "Doe"
+  };
+  alert(person.firstName);
 }
 // JS Objects exercise 2
-        const person = {
-        firstName: "John",
-        lastName: "Doe",
-      country: "Norway"
-       };
-
-       {
-// JS Objects exercise 3
-       const person =
- {
-  name:"John",age:50
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  country: "Norway"
 };
-alert(person.name+ " is " +person.age);
-       }
 
 {
-// JS Events exercise 1
-<button onclick="alert('Hello')">Click me.</button>
+  // JS Objects exercise 3
+  const person =
+  {
+    name: "John", age: 50
+  };
+  alert(person.name + " is " + person.age);
 }
 
 {
-//JS Events exercise 2
-<button onclick="myFunction()">Click me.</button>
+  // JS Events exercise 1
+  <button onclick="alert('Hello')">Click me.</button>
 }
 
 {
-//JS Events exercise 3
-<div onmouseover="this.style.backgroundColor='red'">myDIV.</div>
+  //JS Events exercise 2
+  <button onclick="myFunction()">Click me.</button>
 }
 
 {
-// JS strings exercise 1
-let txt = "Hello World!";
-let x = txt.length;
-alert(x);
+  //JS Events exercise 3
+  <div onmouseover="this.style.backgroundColor='red'">myDIV.</div>
 }
 
 {
-// JS strings exercise 2
-let txt = "We are \"Vikings\"";
-alert(txt);
+  // JS strings exercise 1
+  let txt = "Hello World!";
+  let x = txt.length;
+  alert(x);
 }
 
 {
-    // JS strings exercise 3
-let str1 = "Hello ";
-let str2 = "World!";
-alert(str1+str2);
+  // JS strings exercise 2
+  let txt = "We are \"Vikings\"";
+  alert(txt);
 }
 
 {
-    // JS String Methods exercise 1
-let txt = "Hello World!";
-txt = txt.
-toUpperCase();
+  // JS strings exercise 3
+  let str1 = "Hello ";
+  let str2 = "World!";
+  alert(str1 + str2);
 }
 
 {
-     // JS String Methods exercise 2
-let txt = "I can eat bananas all day";
-let x = txt.slice(10, 17);
+  // JS String Methods exercise 1
+  let txt = "Hello World!";
+  txt = txt.
+    toUpperCase();
 }
 
 {
-    // JS String Methods exercise 3
-    let txt = "Hello World";
-txt = txt.replace("Hello", "Welcome");
+  // JS String Methods exercise 2
+  let txt = "I can eat bananas all day";
+  let x = txt.slice(10, 17);
 }
 
 {
-    // JS String Methods exercise 4
-    let txt = "Hello World";
-txt = txt.toUpperCase();
+  // JS String Methods exercise 3
+  let txt = "Hello World";
+  txt = txt.replace("Hello", "Welcome");
 }
 
 {
-    // JS String Methods exercise 5
-    let txt = "Hello World";
-txt = txt.toLowerCase();
+  // JS String Methods exercise 4
+  let txt = "Hello World";
+  txt = txt.toUpperCase();
+}
+
+{
+  // JS String Methods exercise 5
+  let txt = "Hello World";
+  txt = txt.toLowerCase();
+}
+
+{
+  // JS Arrays exercise 1
+  const cars = ["Saab", "Volvo", "BMW"];
+  let x =cars[1];
 }
